a44c111e1b66224af4d0b1cd437f8f7363be61be
Elena Calvillo <hello@elenacalvillo.com>
2022-12-19 01:29:09 +0000

    better removing all arrows

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: '<- App', href: '/' },
+  { name: 'App', href: '/' },
 ]
 
 export default function About() {
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'About ->', href: '/about' },
+  { name: 'About', href: '/about' },
 ]
 
 export default function Home() {
